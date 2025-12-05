cf4a3cd1762bafc135e337436851fd9f76664dc1
d4ngv0 <24520259@gm.uit.edu.vn>
2025-12-06 00:24:36 +0700

    add function to add score to leaderboard and local,

--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <conio.h>
 #include <windows.h>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <stack>
+
 using namespace std;
 #define H 20
 #define W 15
@@ -37,6 +42,8 @@ char blocks[][4][4] = {
 };
 
 const int tick = 50; // 20 fps
+string localDic = "scoreboards.txt";
+string apiURL = "https://script.google.com/macros/s/AKfycbyBSofw1Jugm68awOHDcthLfNzTuGC_2rxkbTafpgLc3w1NIfnHKwvJmOfIC_0FEuoX/exec";
 int speed = 1000;
 int currentSpeed = 0;
 int level = 0;
@@ -163,6 +170,104 @@ bool canFall(){
         fall = true;
     }
     return fall;
+}
+
+string execCurl(string cmd) {
+    string result;
+    char buffer[256];
+
+    FILE* pipe = _popen(cmd.c_str(), "r");
+    if (!pipe) return "ERROR";
+
+    while (fgets(buffer, sizeof(buffer), pipe) != NULL) {
+        result += buffer;
+    }
+
+    _pclose(pipe);
+    return result;
+}
+
+void addScore2LocalLdb(string name, int score){
+    ofstream ost;
+    ost.open(localDic, ios::out | ios::app);
+    ost<<name<<" "<<score<<"\n";
+    ost.close();
+}
+
+void deleteLocalLdb(){
+    ofstream ost(localDic);
+    ost<<"";
+    ost.close();
+}
+
+void addScore2GlobalLdb(string name, int score){
+    string data = "\"name="+name+"&score="+to_string(score)+"\" ";
+    try {
+        string json = execCurl("curl -d " + data + apiURL);
+    } catch (...){
+        cout<<"Error! Can't push score to global leaderboard!\n";
+    }
+}
+
+string get10GlobalLdb(int page){
+    string json ="{}";
+    try {
+        json = execCurl("curl -L -s \"" + apiURL + "?page=" + to_string(page) + "\"");
+    } catch (...){
+        cout<<"Error! Can't get scores from global leaderboard!\n";
+    }
+    return json;
+}
+
+string get10LocalLdb(){
+    string name, score;
+    ifstream ifs(localDic);
+    while (ifs>>name>>score>>date){
+
+    }
+
+    ifs.close();
+    return "";
+}
+
+vector<vector<string>> processJSON(string json){
+    vector<vector<string>> datas;
+    stack<char> st;
+    int i = 1;
+    st.push(json[0]);
+    vector<string> line;
+    string data = "";
+
+    while (!st.empty()){
+        if (json[i] == ']'){
+            if (st.size() > 1){
+                line.push_back(data);
+                data = "";
+                datas.push_back(line);
+                line.clear();
+            }
+            st.pop();
+        } else if (json[i] == '['){
+            st.push(json[i]);
+        } else if (json[i] == ','){
+            if (st.size() > 1){
+                line.push_back(data);
+                data = "";
+            }
+        } else{
+            data += json[i];
+        }
+
+        i++;
+    }
+
+    return datas;
+}
+
+void debug(vector<vector<string>> datas){
+    for (int i = 0; i < datas.size(); i++){
+        cout<<datas[i][0]<<" "<<datas[i][1]<<" "<<datas[i][2]<<"\n";
+    }
 }
 
 int main()
